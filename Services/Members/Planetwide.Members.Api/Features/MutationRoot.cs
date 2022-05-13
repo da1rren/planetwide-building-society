acbe37c4897c47c1cb703eb086fbe2f674d78550
Darren Maddox <da1rren@gmail.com>
2022-05-13 12:49:04 +0100

    Reorg in prep for docker compose

--- Services/Members/Planetwide.Members.Api/Features/MutationRoot.cs
+++
@@ -1,9 +0,0 @@
-namespace Planetwide.Members.Api.Features;
-
-/// <summary>
-/// This entity acts as our root node, from here we hang each of mutations off it via annotations
-/// </summary>
-public class MutationRoot
-{
-    public string Version => "Demo";
-}
--- Services/Members/Planetwide.Members.Api/Features/QueryRoot.cs
+++
@@ -1,9 +0,0 @@
-namespace Planetwide.Members.Api.Features;
-
-/// <summary>
-/// This entity acts as our root node, from here we hang each of entities off it via queries
-/// </summary>
-public class QueryRoot
-{
-    public string Version => "Demo";
-}
